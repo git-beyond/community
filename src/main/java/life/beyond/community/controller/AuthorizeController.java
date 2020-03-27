5acad5e37a4f2f355541fac3c7ffef5cf43158e2
beyond <1351979987@qq.com>
2020-03-27 19:44:17 +0800

    extract github config from code to properties

--- src/main/java/life/beyond/community/controller/AuthorizeController.java
+++ src/main/java/life/beyond/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.beyond.community.dto.AccessTokenDTO;
 import life.beyond.community.dto.GithubUser;
 import life.beyond.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name ="code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("02919f844e9595886554");
-        accessTokenDTO.setClient_secret("ededf522f04a94aca286edf658bcdd6cb45b9719");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
--- src/main/java/life/beyond/community/provider/GithubProvider.java
+++ src/main/java/life/beyond/community/provider/GithubProvider.java
@@ -4,18 +4,19 @@ import com.alibaba.fastjson.JSON;
 import life.beyond.community.dto.AccessTokenDTO;
 import life.beyond.community.dto.GithubUser;
 import okhttp3.*;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
+
 
 import java.io.IOException;
 
-@Controller
+@Component
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
-                .url("http://github.com/login/oauth/access_token")
+                .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
                 try(Response response = client.newCall(request).execute()){
@@ -31,7 +32,7 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("http://api.github.com/user?access_token=" + accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
